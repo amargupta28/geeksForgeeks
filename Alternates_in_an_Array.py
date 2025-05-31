b1780bbaf87d10646c27d0fde661eb163d7c0d20
Amar Gupta <amar17gupta@gmail.com>
2025-05-31 00:34:04 -0500

    Fix filenames: replace spaces with underscores and add .py where missing

--- Alternates_in_an_Array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def getAlternates(self, arr):
-        # Code Here
-        new=[]
-        for i in range(0,len(arr),2):
-            new.append(arr[i])
-        return new
--- Largest_Element_in_Array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def largest(self, arr):
-        # code here
-        max=0
-        for i in arr:
-            if i > max:
-                max = i
-        return max
